36bc87596b46a43d8f5f338783250fae1b4c9d41
lamdayne123 <acccuoi400@gmail.com>
2026-08-30 15:45:13 +0700

    Update music.ts

--- data/music.ts
+++ data/music.ts
@@ -6,8 +6,28 @@ export interface Song {
 }
 
 export const playlist: Song[] = [
-  { title: "夜に駆ける", artist: "YOASOBI", duration: "4:21", cover: "/images/music/yoasobi.jpg" },
-  { title: "花に亡霊", artist: "ヨルシカ", duration: "4:01", cover: "/images/music/yorushika.jpg" },
-  { title: "アイドル", artist: "YOASOBI", duration: "3:33", cover: "/images/music/idol.jpg" },
-  { title: "光へ", artist: "Aimer", duration: "4:50", cover: "/images/music/aimer.jpg" }
+  {
+    title: "夜に駆ける",
+    artist: "YOASOBI",
+    duration: "4:21",
+    cover: "/images/music/yoasobi.jpg"
+  },
+  {
+    title: "花に亡霊",
+    artist: "ヨルシカ",
+    duration: "4:01",
+    cover: "/images/music/yorushika.jpg"
+  },
+  {
+    title: "アイドル",
+    artist: "YOASOBI",
+    duration: "3:33",
+    cover: "/images/music/idol.jpg"
+  },
+  {
+    title: "光へ",
+    artist: "Aimer",
+    duration: "4:50",
+    cover: "/images/music/aimer.jpg"
+  }
 ];
