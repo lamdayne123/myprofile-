42d0c098cbb9be6f00309702f65c0bb1744fb93a
lamdayne123 <acccuoi400@gmail.com>
2026-08-31 13:31:31 +0700

    Delete app directory

--- app/layout.tsx
+++
@@ -1,3 +0,0 @@
-import "./globals.css";
-export const metadata={title:"Trương Chí Lâm",description:"Personal profile"};
-export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="vi"><body>{children}</body></html>}
