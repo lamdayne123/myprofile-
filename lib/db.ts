77a1f4e8e90a1d61aa12bd531028bb42a1700299
lamdayne123 <acccuoi400@gmail.com>
2026-08-31 13:27:43 +0700

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -2,10 +2,6 @@ import { neon } from "@neondatabase/serverless";
 
 const databaseUrl = process.env.DATABASE_URL;
 
-if (!databaseUrl) {
-  throw new Error(
-    "DATABASE_URL chưa được cấu hình trong .env.local"
-  );
-}
-
-export const sql = neon(databaseUrl);
+export const sql = databaseUrl
+  ? neon(databaseUrl)
+  : null;
