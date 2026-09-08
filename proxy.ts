999d3800650f73905425dd5b4209b93e48efad31
lamdayne123 <acccuoi400@gmail.com>
2026-09-08 17:23:05 +0700

    Add files via upload

--- proxy.ts
+++ proxy.ts
@@ -1,3 +1,4 @@
+/* proxy.ts */
 import { type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/proxy";
 
@@ -7,6 +8,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|avif)$).*)",
   ],
 };
